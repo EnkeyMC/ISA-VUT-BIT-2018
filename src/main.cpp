ebce8aa115fc8b55bfb729116cc26e77ba0f0b09
EnkeyMC <enkeymc@gmail.com>
2018-10-03 18:55:21 +0200

    OpenSSL linked

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
 #include <libxml/xmlreader.h>
+#include <openssl/bio.h>
+#include <openssl/ssl.h>
+#include <openssl/err.h>
 
 int main() {
+    SSL_load_error_strings();
+    ERR_load_BIO_strings();
+    OpenSSL_add_all_algorithms();
+
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
